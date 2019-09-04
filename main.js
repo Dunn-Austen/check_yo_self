17ccc87bd7a919e79f9b94639b318e6ccc9ad89f
Jesse Maxim <jessmaxim303@gmail.com>
2019-09-04 15:05:59 -0600

    Completed function and style

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 // Global Variables
 var sectionLeft = document.querySelector(".left__main--section");
+var jsTitleInput = document.querySelector(".js__title--input");
+var jsItemInput = document.querySelector(".js__item--input");
+var jsPlusButton = document.querySelector(".js__plus--button");
+var jsCardArea = document.querySelector(".js__card--area");
+var jsListButton = document.querySelector(".js__list--button");
 
 // Event Listener for left section, initiates insertTaskItem function upon click
 sectionLeft.addEventListener('click', eventHandlerTopLeft);
+jsListButton.addEventListener('click', makeTaskCard);
 
 // Named function for eventListener
 function eventHandlerTopLeft() {
@@ -24,7 +30,7 @@ function insertLeftTaskItem() {
       newItemLeftSection.classList.add("js__dynamic--insert");
       newItemLeftSection.innerHTML =
       `
-      <img class="js__delete--icon" src="images/search.svg" alt="Delete Icon for removing task item">
+      <img class="js__delete--icon" src="images/delete.svg" alt="Delete Icon for removing task item">
         <span>${taskItemValue}</span>
       `
     }
@@ -36,3 +42,23 @@ function deleteLeftTaskItem() {
   var removeTaskItem = document.querySelector('.js__dynamic--insert');
   removeTaskItem.remove();
   }
+
+function makeTaskCard() {
+  console.log('make task card!')
+    jsCardArea.insertAdjacentHTML('afterbegin',
+    `<container class="js__card--container">
+      <section class="js__task--card">
+        <h3 class="js__h3--card">${jsTitleInput.value}</h3>
+        <div class="task__body--container">
+          <div class="task__container">
+            <img class="js__task--urgent icons" src="images/checkbox.svg" alt="Checkbox for completed tasks">
+            <section class="task__item">${jsItemInput.value}</section>
+          </div>
+        </div>
+          <div class="task__card--icon">
+            <img class="js__task--urgent icon" src="images/urgent.svg" alt="Update urgency on card">
+            <img class="js__task--delete icon" src="images/delete.svg" alt="Delete card">
+          </div>
+      </section>
+     </container>`);
+}
