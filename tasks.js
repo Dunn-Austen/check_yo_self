9c9c3ebaac223b8941f800a0fab9f25bb507f23a
Jesse Maxim <45706207+Jessmaxim303@users.noreply.github.com>
2019-09-08 14:43:42 -0600

    Revert "Tasks array"

--- tasks.js
+++
@@ -1,7 +0,0 @@
-class Task {
-	constructor(tasks){
-		this.task = tasks.name;
-		this.id = tasks.id;
-    // this.name = tasks.name;
-	}
-}
--- todo-list.js
+++
@@ -1,7 +0,0 @@
-class TodoList {
-	constructor(task){
-		this.id = task.id;
-		this.title = task.title;
-    this.taskList = task.taskList;
-	}
-}
