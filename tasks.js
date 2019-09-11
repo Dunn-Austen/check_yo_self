eb5936d3378ce5bf02c301b7eb55943e625cec24
Jesse Maxim <jessmaxim303@gmail.com>
2019-09-10 23:14:48 -0600

    Added the findId function and set up todo data model

--- tasks.js
+++ tasks.js
@@ -2,5 +2,26 @@ class Task {
     constructor(tasks){
         this.id = tasks.id;
         this.task = tasks.task;
+        this.complete = false;
     }
+
+    saveToStorage() {
+
+    }
+
+    deleteFromStorage() {
+
+    }
+
+// (should update the to-do’s title and urgency
+		updateToDo() {
+
+		} 
+
+// (should update a task’s content and if it has been completed)
+		updateTask() {
+
+		}
+
+
 }
--- todo-list.js
+++ todo-list.js
@@ -2,21 +2,12 @@ class TodoList {
     constructor(task){
       this.id = task.id;
       this.title = task.title;
+      this.urgent = false;
       this.tasks = task.tasks;
     }
-    updateTask(event){
-      console.log("Method Running")
-      if (event.target.src === "http://127.0.0.1:3000/images/checkbox.svg"){
-          console.log("Get Here?")
-          event.target.src = "http://127.0.0.1:3000/images/checkbox-active.svg";
-          event.target.parentElement.classList.add('checkoff_task');
-      } else if (event.target.src === "http://127.0.0.1:3000/images/checkbox-active.svg"){
-            event.target.src = "http://127.0.0.1:3000/images/checkbox.svg";
-            event.target.parentElement.classList.remove("checkoff_task");
-      } else return
-    }
+
+
 
 }
-// http://127.0.0.1:3000/images/checkbox.svg
 
 // https://dunn-austen.github.io/check_yo_self/images/checkbox.svg
