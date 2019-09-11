268ce16fe8a3842e261af697afed20d813cfc9e6
Alan Turing <austeyd@gmail.com>
2019-09-10 19:39:06 -0600

    Add urgent functionality and partial css polishing for media query

--- todo-list.js
+++ todo-list.js
@@ -4,6 +4,7 @@ class TodoList {
       this.title = task.title;
       this.tasks = task.tasks;
     }
+    
     updateTask(event){
       console.log("Method Running")
       if (event.target.src === "http://127.0.0.1:3000/images/checkbox.svg"){
@@ -16,7 +17,16 @@ class TodoList {
       } else return
     }
 
-}
-// http://127.0.0.1:3000/images/checkbox.svg
+    toggleUrgent(event) {
+      if (event.target.src === "http://127.0.0.1:3000/images/urgent.svg") {
+        event.target.src = "http://127.0.0.1:3000/images/urgent-active.svg";
+        event.target.parentElement.parentElement.classList.add('js__task--card--urgent');
+      } else if (event.target.src = "http://127.0.0.1:3000/images/urgent-active.svg") {
+            event.target.src = "http://127.0.0.1:3000/images/urgent.svg";
+            event.target.parentElement.parentElement.classList.remove("js__task--card--urgent");
+        }
+      }
+    }
+// http://127.0.0.1:3000/images/active-lightning.svg
 
 // https://dunn-austen.github.io/check_yo_self/images/checkbox.svg
